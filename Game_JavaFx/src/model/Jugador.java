4aa2fa7388fb7a1f49e36c55780a26df3c67d442
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-10 15:29:55 -0500

    Update Class file
    Update Class file -> Jugador

--- Game_JavaFx/src/model/Jugador.java
+++ Game_JavaFx/src/model/Jugador.java
@@ -3,25 +3,25 @@ package model;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Random;
 
-public class Jugador extends Juego {
-	
+public class Jugador extends Juego {	
 	protected int puntaje;
 	protected String codigoMorse;
-	protected boolean juegoActivo;
+	protected boolean juegoActivo;	
+	Jugador jugador1 = new Jugador();
+	Scanner scanner = new Scanner(System.in);
 
-	public void jugar () {
-		
-		Jugador jugador1 = new Jugador();
+	public void jugar () {				
 		jugador1.segundos = 60;
 		juegoActivo = true;
 		puntaje = 0;
 		codigoMorse ="";
-		conteoRegresivo (jugador1.segundos);		
+		iniciarConteoRegresivo (jugador1.segundos);	
+		iniciarPartida ();
 	}
 	
-	public void conteoRegresivo (int tempo) {
-		
+	public void iniciarConteoRegresivo (int tempo) {		
         Timer temporizador = new Timer();
         temporizador.schedule(new TimerTask() {
             @Override
@@ -35,6 +35,31 @@ public class Jugador extends Juego {
 
 	}
 	
+	public void iniciarPartida () {		
+		while (juegoActivo) {			
+			int posicion = obtenerPosicionAleatoria();
+			System.out.println(jugador1.tablaPalabras[posicion][0]);
+			codigoMorse = capturarCodigoMorse ();
+			if(compararCodigos(codigoMorse, posicion)) {
+				puntaje = puntaje+10;
+			}
+		}		
+	}
 	
+	
+	public int obtenerPosicionAleatoria () {		
+		Random random = new Random();
+		return random.nextInt(100);		
+	}
+	
+	
+	public String capturarCodigoMorse () {		
+		System.out.print("Escriba el codigo Morse de la palabra: ");
+		return scanner.nextLine();		
+	}
+	
+	public boolean compararCodigos (String codigo, int fila) {
+		return  jugador1.tablaPalabras[fila][0].equalsIgnoreCase(codigo);
+	}
 
 }
