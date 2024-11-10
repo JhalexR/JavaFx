212d09849993214ba281a4965a759bda7ef6c092
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-10 14:13:58 -0500

    Update Class file
    Update Class file -> Jugador

--- Game_JavaFx/src/model/Jugador.java
+++ Game_JavaFx/src/model/Jugador.java
@@ -1,10 +1,39 @@
 package model;
 
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Jugador extends Juego {
 	
 	protected int puntaje;
 	protected String codigoMorse;
+
+	public void jugar () {
+		
+		Jugador jugador1 = new Jugador();
+		jugador1.segundos = 60;
+		puntaje = 0;
+		codigoMorse ="";
+		conteoRegresivo ();		
+	}
 	
-	
+	public void conteoRegresivo () {
+		
+		Jugador jugador1 = new Jugador();
+		jugador1.segundos = 60;
+				
+	       new Thread(() -> {
+	            while (jugador1.segundos > 0) {
+	                try {
+	                    Thread.sleep(1000);  // Esperar 1 segundo
+	                    jugador1.segundos--;
+	                } catch (InterruptedException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	            finalizarJuego();
+	        }).jugar();
+	}
 
 }
