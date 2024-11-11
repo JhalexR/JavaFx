32ef4caaea39b977daf33f29783da182ad6f4fb8
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-11 11:14:06 -0500

    Update class files
    Update class files

--- Game_JavaFx/src/model/Inicio.java
+++ Game_JavaFx/src/model/Inicio.java
@@ -2,37 +2,43 @@ package model;
 
 public class Inicio extends Juego {
 	
-	public void MostrartablaLetras () {			
-		for(int i=0; i<26; i++) {
-			for(int j=0; j<2; j++) {
-				System.out.print(tablaLetras[i][j]+" ");
-			}
-			System.out.println();
-		}	
+	protected static String [][] letras;
+	protected static String [][] palabras;
+	
+	public Inicio(){
 		
-		for(int i=0; i<100; i++) {
-			for(int j=0; j<2; j++) {
-				System.out.print(tablaPalabras[i][j]+" ");
+		Juego juego1 = new Juego();
+		juego1.iniciar();
+		
+		letras = new String [26][2];
+		palabras = new String [100][2];
+		
+		for(int j=0; j<2; j++) {
+			for(int i=0; i<26; i++) {
+				letras[i][j] = juego1.tablaLetras[i][j];
 			}
-			System.out.println();
-		}	
-	}
+		}
+		
+		for(int j=0; j<2; j++) {
+			for(int i=0; i<100; i++) {
+				palabras[i][j] = juego1.tablaPalabras[i][j];
+			}
+		}
+	}	
 	
 	public void MostrarInstrucciones () {				
 		System.out.println("El juego consiste en digitar el Codigo Morse de la palabra ");
 		System.out.println("que aparece en el centro, usando como guia la tabla con la traduccion");
 		System.out.println("el codigo morse solo usa los simbolos ( . ) y ( - )");
 		System.out.println("se debe digitar el Código Morse sin espacios y al terminar la traducción");
-		System.out.println("se debe presioanr (enter) para pasar a la siguiente palabra ");				
+		System.out.println("se debe presionar (enter) para pasar a la siguiente palabra ");				
 	}
 	
-
-	public static void main(String[] args) {				
+	public static void main(String[] args) {	
 		Inicio partida = new Inicio();		
-		partida.llenarTablas();
-		partida.MostrartablaLetras();
 		partida.MostrarInstrucciones();		
-		Jugador partida2 = new Jugador();		
-		partida2.jugar();		
+
+		Jugador Jugador1 = new Jugador();
+		Jugador1.jugar(letras, palabras);		
 	}
 }
--- Game_JavaFx/src/model/Jugador.java
+++ Game_JavaFx/src/model/Jugador.java
@@ -5,22 +5,53 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Random;
 
-public class Jugador extends Juego {	
+public class Jugador extends Inicio {	
 	protected int puntaje;
 	protected String codigoMorse;
-	protected boolean juegoActivo;	
-
-	public void jugar () {	
-		Jugador jugador1 = new Jugador();
-		jugador1.segundos = 60;
-		juegoActivo = true;
+	protected boolean juegoActivo;
+	protected static String [][] listaLetras; 
+	protected static String [][] listaPalabras; 
+		
+	Scanner scanner = new Scanner(System.in);
+	
+	public void jugar (String letrasM[][], String palabrasM[][]) {	
+		segundos = 60;
 		puntaje = 0;
-		codigoMorse ="";
-		iniciarConteoRegresivo (jugador1.segundos);	
-		iniciarPartida ();
+		codigoMorse ="";			
+		juegoActivo = true;
+		listaLetras = new String [26][2];
+		listaPalabras = new String [100][2];
+		
+		for(int j=0; j<2; j++) {
+			for(int i=0; i<26; i++) {
+				listaLetras[i][j] = letrasM[i][j];
+			}
+		}
+		
+		for(int j=0; j<2; j++) {
+			for(int i=0; i<100; i++) {
+				listaPalabras[i][j] = palabrasM[i][j];
+			}
+		}
+		
+		mostrartablaLetras();
+		iniciarConteoRegresivo (segundos);	
+		iniciarPartida ();		
+		scanner.close();
 	}
 	
-	public void iniciarConteoRegresivo (int tempo) {		
+	public void mostrartablaLetras () {			
+		for(int i=0; i<26; i++) {
+			for(int j=0; j<2; j++) {
+				System.out.print(listaLetras[i][j]+" ");
+			}
+			System.out.println();
+		}	
+		System.out.println();
+	}
+	
+	public void iniciarConteoRegresivo (int tempo) {	
+		
         Timer temporizador = new Timer();
         temporizador.schedule(new TimerTask() {
             @Override
@@ -29,23 +60,28 @@ public class Jugador extends Juego {
                 juegoActivo = false;
                 System.out.println("\n¡Tiempo agotado!");
                 temporizador.cancel();
+        		finalizarPartida ();
             }
         }, tempo * 1000);
 
 	}
 	
-	public void iniciarPartida () {		
-		Juego jugadorA = new Juego();
+	public void iniciarPartida () {	
 		while (juegoActivo) {			
 			int posicion = obtenerPosicionAleatoria();
-			System.out.println(posicion);
-			int a = 0;
-			System.out.println(jugadorA.tablaPalabras[posicion][a]);
+			System.out.println("\nLa palabra a traducir es :");
+			System.out.println(listaPalabras[posicion][0]);						
 			codigoMorse = capturarCodigoMorse();
 			if(compararCodigos(codigoMorse, posicion)) {
-				puntaje = puntaje+10;
+				puntaje = puntaje+10;				
+				System.out.println("\nCodigo Morse Correcto");
+				System.out.println("Se suman puntos +10 puntos");
 			}
-		}		
+			else {
+				System.out.println("\nCodigo Morse Incorrecto");
+				System.out.println("No se suman puntos");
+			}
+		}
 	}
 	
 	
@@ -55,15 +91,20 @@ public class Jugador extends Juego {
 	}
 	
 	
-	public String capturarCodigoMorse () {		
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Escriba el codigo Morse de la palabra: ");
+	public String capturarCodigoMorse () {	
+		System.out.println("Escriba el codigo Morse de la palabra:");
 		return scanner.nextLine();		
 	}
 	
 	public boolean compararCodigos (String codigo, int fila) {
-		Jugador jugador1 = new Jugador();
-		return  jugador1.tablaPalabras[fila][0].equalsIgnoreCase(codigo);
+		return  listaPalabras[fila][1].equalsIgnoreCase(codigo);
+	}
+	
+	public void finalizarPartida () {
+		System.out.println("\n¡Su puntaje fue de!\n");
+		System.out.println(puntaje);
+		System.out.println("\nPresione enter para finalizar\n");
+		scanner.nextLine();
 	}
 
 }
