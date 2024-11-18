66b3b5844447b0ded74f194a46f568d21ba8604f
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-18 15:36:25 -0500

    refactor JUnit class
    refactor JUnit Jugadortest class

--- Game_JavaFx/src/co/edu/poli/test/Jugadortest.java
+++ Game_JavaFx/src/co/edu/poli/test/Jugadortest.java
@@ -6,42 +6,103 @@ import model.Jugador;
 /*
 	Clase que realiza las pruebas unitarias para los metodos:
 	* obtenerPosicionAleatoria ();
-	* capturarCodigoMorse ();
 	* compararCodigos (String, int);
-	Debido a que son los únicos métodos con retorno de valores  
+	Debido a que son dos de los tres métodos con retorno de valores  
 */
 public class Jugadortest extends TestCase {
 	
 	/*
 	  Test que realiza la prueba del método ObtenerPosicionAleatoria ();
-	  que debe generar un numero aleatorio entre 0 y 99
-	  esta prueba no debe generar el mensaje  "La posición debe estar entre 0 y 99"
-	  para considerar que el metodo funciona correctamente	  
-	*/
+	  el resultado debe ser igual al esperado para considerar 
+	  que el metodo funciona correctamente 
+	*/    
     public void testObtenerPosicionAleatoria() {
-        Jugador jugador = new Jugador();
-        for (int i = 0; i < 10; i++) { 
-            int posicion = jugador.obtenerPosicionAleatoria();
-            assertTrue("La posición debe estar entre 0 y 99", posicion >= 0 && posicion < 100);
+        String[][] letras = new String[27][2];
+        String[][] palabras = new String[100][2];
+        Jugador jugador = new Jugador(letras, palabras);
+        for (int i = 0; i < 10; i++) {
+        	int posicion = jugador.obtenerPosicionAleatoria();
+        	assertTrue("La posición debe estar entre 0 y 99", posicion >= 0 && posicion < 100);
         }
     }
     
 	/*
-	  Test que realiza la prueba del método ObtenerPosicionAleatoria ()
-	  que debe generar un numero aleatorio entre 0 y 99
-	  esta prueba debe generar siempre el mensaje  "La posición debe estar entre 0 y 99"
-	  puesto que se suma +100 al resultado demostrando que el metodo genera numeros dentro del rango	  
+	  Test que realiza la prueba del método ObtenerPosicionAleatoria
+	  el resultado debe ser diferente al esperado para considerar 
+	  que el metodo funciona correctamente	  
 	*/
     public void testObtenerPosicionAleatoriaIncorrecto() {
-        Jugador jugador = new Jugador();
-        for (int i = 0; i < 10; i++) { 
-            int posicion = jugador.obtenerPosicionAleatoria()+100;
-            assertTrue("La posición debe estar entre 0 y 99", posicion >= 0 && posicion < 100);
-      }
-  }
-    
-    public void testcompararCodigos() {
-    	Jugador.jugar();
+        String[][] letras = new String[27][2];
+        String[][] palabras = new String[100][2];
+        Jugador jugador = new Jugador(letras, palabras);
+        for (int i = 0; i < 10; i++) {
+        	int posicion = jugador.obtenerPosicionAleatoria()+100;
+        	assertTrue("La posición debe estar entre 0 y 99", posicion >= 0 && posicion < 100);
+        }
     }
+    
+	/*
+	  Test que realiza la prueba del método CompararCodigos
+	  el resultado debe ser igual al esperado para considerar 
+	  que el metodo funciona correctamente 
+	*/
+    public void testCompararCodigos() {
+       String[][] letras = new String[27][2];
+       String[][] palabras = new String[100][2];
+       String[] codigoMorse = new String[4];
+
+       palabras[0][0] = "HOLA";
+       palabras[0][1] = "....---.-...-"; 
+       palabras[1][0] = "JUEGO";
+       palabras[1][1] = "·−−−··−·−−·−−−"; 
+       palabras[2][0] = "QUESO";
+       palabras[2][1] = "−−·−··−····−−−"; 
+       palabras[3][0] = "AMOR";
+       palabras[3][1] = "·−−−−−−·−·";
+              
+       codigoMorse [0] = "....---.-...-";
+       codigoMorse [1] = "·−−−··−·−−·−−−"; 
+       codigoMorse [2] = "−−·−··−····−−−";
+       codigoMorse [3] = "·−−−−−−·−·";
+
+       Jugador jugador = new Jugador(letras, palabras);
+       
+		for(int i=0; i<4; i++) {
+			    boolean resultadoCorrecto = jugador.compararCodigos(codigoMorse [i], i);
+			    assertTrue("El código Morse debería coincidir.", resultadoCorrecto);
+			}		
+   }
+    
+	/*
+	  Test que realiza la prueba del método CompararCodigos
+	  el resultado debe ser diferente al esperado para considerar 
+	  que el metodo funciona correctamente 
+	*/
+  public void testCompararCodigosIncorrecto() {
+     String[][] letras = new String[27][2];
+     String[][] palabras = new String[100][2];
+     String[] codigoMorse = new String[4];
+
+     palabras[0][0] = "HOLA";
+     palabras[0][1] = "....---.-...-"; 
+     palabras[1][0] = "JUEGO";
+     palabras[1][1] = "·−−−··−·−−·−−−"; 
+     palabras[2][0] = "QUESO";
+     palabras[2][1] = "−−·−··−····−−−"; 
+     palabras[3][0] = "AMOR";
+     palabras[3][1] = "·−−−−−−·−·";
+            
+     codigoMorse [0] = "............";
+     codigoMorse [1] = "------------"; 
+     codigoMorse [2] = "abdefghijklm";
+     codigoMorse [3] = "123456789";
+
+     Jugador jugador = new Jugador(letras, palabras);
+     
+		for(int i=0; i<4; i++) {
+		     boolean resultadoIncorrecto = jugador.compararCodigos(codigoMorse [i], i);
+		     assertFalse( "El código Morse no debería coincidir.", resultadoIncorrecto);
+			}		
+  	}
     
 }
