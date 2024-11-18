4236695419a351faa2e267d14f30ea51b7f0e11b
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-18 17:28:48 -0500

    Genrated JavaDocs files
    Genrated JavaDocs files package model

--- Game_JavaFx/src/model/Juego.java
+++ Game_JavaFx/src/model/Juego.java
@@ -22,7 +22,7 @@ public class Juego {
 	}
 	
     /**
-    Método que llena la tabla de letras con cada letra del alfabeto espanol 
+    Metodo que llena la tabla de letras con cada letra del alfabeto espanol 
     y su traduccion correspondiente en codigo Morse.
    */
 	public void llenarTablaLetras () {
