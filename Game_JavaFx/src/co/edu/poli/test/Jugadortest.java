632a4c60a32c570c2c08cd2f15a7aaed78adffd3
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-11-18 10:43:30 -0500

    JUnits class Jugador
    Add test -> method -> capturarCodigoMorse

--- Game_JavaFx/src/co/edu/poli/test/Jugadortest.java
+++ Game_JavaFx/src/co/edu/poli/test/Jugadortest.java
@@ -1,13 +1,14 @@
 package co.edu.poli.test;
 
 import junit.framework.TestCase;
+import java.io.ByteArrayInputStream;
 import model.Jugador;
 
 /*
 	Clase que realiza las pruebas unitarias para los metodos:
 	* obtenerPosicionAleatoria ();
 	* capturarCodigoMorse ();
-	* compararCodigos ();
+	* compararCodigos (String, int);
 	Debido a que son los únicos métodos con retorno de valores  
 */
 public class Jugadortest extends TestCase {
@@ -27,7 +28,7 @@ public class Jugadortest extends TestCase {
     }
     
 	/*
-	  Test que realiza la prueba del método ObtenerPosicionAleatoria ();
+	  Test que realiza la prueba del método ObtenerPosicionAleatoria ()
 	  que debe generar un numero aleatorio entre 0 y 99
 	  esta prueba debe generar siempre el mensaje  "La posición debe estar entre 0 y 99"
 	  puesto que se suma +100 al resultado demostrando que el metodo genera numeros dentro del rango	  
@@ -39,4 +40,19 @@ public class Jugadortest extends TestCase {
             assertTrue("La posición debe estar entre 0 y 99", posicion >= 0 && posicion < 100);
       }
   }
+    
+	/*
+	  Test que realiza la prueba del método capturarCodigoMorse()
+	  que debe generar un numero aleatorio entre 0 y 99
+	  esta prueba debe generar siempre el mensaje  "La posición debe estar entre 0 y 99"
+	  puesto que se suma +100 al resultado demostrando que el metodo genera numeros dentro del rango	  
+	*/
+    public void testcapturarCodigoMorse() {
+        String input = "... --- ...\n"; // Entrada simulada (SOS en código Morse)
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in); // Simula la entrada estándar
+        Jugador jugador = new Jugador();
+        String codigo = jugador.capturarCodigoMorse();
+        assertEquals("... --- ...", codigo, "El código Morse capturado debe coincidir con la entrada simulada");
+    }
 }
